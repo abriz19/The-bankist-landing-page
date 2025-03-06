1a2f28a963c0401c005e718537e76d1a54477243
Abreham Dessu <abrizdessu@gmail.com>
2025-03-06 00:05:37 -0800

    feat: implement a way of revealing page sections on event scroll

--- script.js
+++ script.js
@@ -117,3 +117,22 @@ const handleHover = function (e) {
 const navEl = document.querySelector('.nav');
 navEl.addEventListener('mouseover', handleHover.bind(0.5));
 navEl.addEventListener('mouseout', handleHover.bind(1));
+
+// Revealing sections on scroll
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
